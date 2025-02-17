686d2e6cc71fbdc368a391dc9a798a24be3aee43
Wutthiphon <woodychgamer5588@gmail.com>
2025-02-17 10:28:11 +0700

    Update landing page design and app title; add logo asset

--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -10,9 +10,42 @@ class LandingPage extends StatefulWidget {
 class _LandingPageState extends State<LandingPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Hello, World!'),
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topRight,
+          end: Alignment.bottomLeft,
+          colors: [
+            Color(0xFF344754),
+            Color(0xFFd4cfc9),
+          ],
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ClipRRect(
+                borderRadius: BorderRadius.circular(25),
+                child: Image.asset(
+                  'assets/logo/logo.png',
+                  width: 200,
+                ),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'LockLock!',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
