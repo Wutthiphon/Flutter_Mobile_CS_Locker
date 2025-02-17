ade093f30c0de6cca3dfab5bc34b42540bd2efd0
Wutthiphon <woodychgamer5588@gmail.com>
2025-02-17 11:00:42 +0700

    Refactor BottomMenu component to use StatelessWidget instead of StatefulWidget

--- lib/components/app_bottom_menu/bottom_menu.dart
+++ lib/components/app_bottom_menu/bottom_menu.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/material.dart';
 
-class BottomMenu extends StatefulWidget {
+class BottomMenu extends StatelessWidget {
   const BottomMenu({super.key});
 
-  @override
-  State<BottomMenu> createState() => _BottomMenuState();
-}
-
-class _BottomMenuState extends State<BottomMenu> {
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
