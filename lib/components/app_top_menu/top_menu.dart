bc333f4ee676b841884b4223cdfbc3ad51e677b0
Wutthiphon Tassana <woodychgamer5588@gmail.com>
2025-03-04 01:30:59 +0700

    Update TopMenu to use a background image for the HomePage and adjust AppBar height based on active index; clean up redundant code in HomePage

--- lib/components/app_top_menu/top_menu.dart
+++ lib/components/app_top_menu/top_menu.dart
@@ -27,27 +27,31 @@ class TopMenu extends StatelessWidget implements PreferredSizeWidget {
           fontSize: 22,
         ),
       ),
-      // backgroundColor: const Color.fromARGB(255, 59, 86, 102),
-      // gradient background
       backgroundColor: Colors.transparent,
-      flexibleSpace: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Color.fromARGB(255, 110, 132, 145),
-              Color.fromARGB(255, 59, 86, 102),
-            ],
-            begin: Alignment.centerLeft,
-            end: Alignment.centerRight,
-          ),
-        ),
-      ),
+      flexibleSpace: activeIndex == 0
+          ? Image.asset(
+              './assets/background/page-top-left.png',
+              fit: BoxFit.cover,
+            )
+          : Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Color.fromARGB(255, 110, 132, 145),
+                    Color.fromARGB(255, 59, 86, 102),
+                  ],
+                  begin: Alignment.centerLeft,
+                  end: Alignment.centerRight,
+                ),
+              ),
+            ),
       actions: [
         !isSignIn
-            ? IconButton(
+            ? IconButton.outlined(
                 icon: const Icon(Icons.login),
                 style: ButtonStyle(
-                  foregroundColor: WidgetStateProperty.all(Colors.white),
+                  foregroundColor: WidgetStateProperty.all(
+                      activeIndex == 0 ? Colors.blueGrey : Colors.white),
                 ),
                 onPressed: () {
                   Navigator.push(
@@ -64,5 +68,12 @@ class TopMenu extends StatelessWidget implements PreferredSizeWidget {
   }
 
   @override
-  Size get preferredSize => Size.fromHeight(appBar.preferredSize.height);
+  Size get preferredSize {
+    // เพิ่มความสูงให้กับ AppBar เมื่อ activeIndex == 0
+    if (activeIndex == 0) {
+      return Size.fromHeight(appBar.preferredSize.height + 30);
+    } else {
+      return appBar.preferredSize;
+    }
+  }
 }
