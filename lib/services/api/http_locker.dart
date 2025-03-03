22a7a73856ea2db3b92a72a429ba8303a3d49923
Wutthiphon <woodychgamer5588@gmail.com>
2025-03-03 09:25:06 +0700

    Refactor HTTP service classes: move HttpLocker and HttpAuth to new api directory

--- lib/services/api/http_locker.dart
+++ lib/services/api/http_locker.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'data_type.dart';
+import '../data_type.dart';
 
 String host = 'http://localhost:3000/api/lockers';
 
